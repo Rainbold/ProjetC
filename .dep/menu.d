e4fc27c42b9c565d4137ceb10875a5a19d08949e
gatux <gatux94@gmail.com>
2014-05-28 22:27:01 +0200

    Netoyage

--- .dep/menu.d
+++
@@ -1,3 +0,0 @@
-.obj/menu.o: src/menu.c include/game.h include/player.h include/map.h \
- include/constant.h include/level.h include/misc.h include/window.h \
- include/sprite.h include/menu.h
--- .dep/player.d
+++
@@ -1,3 +0,0 @@
-.obj/player.o: src/player.c include/player.h include/game.h \
- include/level.h include/map.h include/constant.h include/sprite.h \
- include/window.h include/misc.h include/wiimote.h
