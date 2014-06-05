91a4d1fca38231727de66ad2b9b173de61495d6f
RainbowLyte <admin@lyte.fr>
2014-06-05 18:35:38 +0200

    Bug affichage ouverture map

--- src/file.c
+++ src/file.c
@@ -57,13 +57,13 @@ struct map* file_read_map1(const char* mapFile) {
 
    struct map* map = map_new(map_width, map_height);
 
-   for(i=0; i<map_width; i++) {
-	   for(j=0; j<map_height; j++) {
-		   if( fread(&byte, 1, sizeof(byte), f) == 0 )
-			   return(NULL);
-		   map_set_cell_type(map, i, j, byte);
-	   }
-   }
+	for(j=0; j<map_height; j++) {
+   		for(i=0; i<map_width; i++) {
+		   	if( fread(&byte, 1, sizeof(byte), f) == 0 )
+				return(NULL);
+		  	map_set_cell_type(map, i, j, byte);
+	   	}
+   	}
 
    fclose(f);
 
