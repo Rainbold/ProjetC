f9df23f7cb35569f60b7097ead627e34cf7fdfe5
RainbowLyte <admin@lyte.fr>
2014-06-05 18:57:07 +0200

    bug fixed

---
+++ .dep/list.d
@@ -0,0 +1,2 @@
+.obj/list.o: src/list.c include/list.h include/map.h include/sprite.h \
+ include/constant.h include/window.h
---
+++ .dep/multi.d
@@ -0,0 +1,3 @@
+.obj/multi.o: src/multi.c include/multi.h include/game.h include/player.h \
+ include/map.h include/constant.h include/level.h include/sprite.h \
+ include/window.h include/misc.h include/bomb.h include/wiimote.h
